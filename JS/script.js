88362966075b25d30b2a5d7f6357b76f8e08ce88
HarishHec7700 <harish.hariharan7@gmail.com>
2022-08-04 16:14:05 +0530

    Java homeworks

---
+++ JS/script.js
@@ -0,0 +1,115 @@
+// // Area of circle
+
+// let r = parseInt(prompt("Give radius"));
+// let a,bi;
+// bi=22/7;
+// a=bi*(r**2);
+// console.log(a);
+// alert(a);
+// document.write(a);
+
+// // Area of triangle
+
+// let base,height,at;
+// base=parsefloat(prompt("Give the base value"));
+// height=parsefloat(prompt("Give the height value"));
+// at=0.5*base*height;
+// console.log(at);
+// alert(at);
+// document.write(at);
+
+// Simple Interest
+
+// let p,n,rate,si;
+// p=parseFloat(prompt("Give the Principal amount"));
+// n=parseFloat(prompt("Give the number of years "));
+// rate=parseFloat(prompt("Give the rate of intrest"));
+// si= (p*n*rate)/100;
+// console.log(si);
+// document.write(si);
+
+// Square , Cube
+
+// let sq,cu, ab=parseFloat(prompt("Give the number to find its square and cube "));
+// sq=ab**2;
+// cu=ab**3;
+// console.log(sq);
+// document.write(sq);
+// console.log(cu);
+// document.write(cu);
+
+// Swap of two numbers 
+
+// let x=parseFloat(prompt("Give the  first number")),y=parseFloat(prompt("Give the secound number")),z;
+// console.log(x);
+// document.write("x=",x);
+// console.log(y);
+// document.write("y=",y);
+// z=x;x=y;y=z;
+// console.log(x);
+// document.write("x=",x);
+// console.log(y);
+// document.write("y=",y);
+
+// To calculate the total and average 
+
+let num=[],n,s=0,sum = 0,i,j;
+n=parseInt(prompt("Enter the total terms"))
+for (i = 0; i < n; i++) {
+    num[i]= parseFloat(prompt("Enter the numbers"));
+}
+for (j = 0; j < n; j++) {
+    sum=s+num[j];
+    s=sum;    
+}
+avg=sum/n;
+console.log(sum);
+document.write("the sum is ",sum);
+console.log(avg);
+document.write("the average is ",avg);
+
+// Find the given number is even or odd
+
+// let gn=parseInt(prompt("Enter the Number to find its odd or even"));
+// if(gn%2==0){
+//     document.write("The given number is an EVEN number");
+// }
+// else{
+//     document.write("The given number is an ODD number");
+
+// }
+
+// To check whether the number is positive or negative
+
+// let gn=parseInt(prompt("Enter the Number to find its positive or negative"));
+// if(gn<0){
+//     document.write("The given number is positive number");
+// }
+// else if(gn==0){
+//     document.write("The given number is Zero");
+// }
+// else{
+//     document.write("The given number is negative number");
+// }
+
+// To find the number is divisible by 5 
+// let gn=parseInt(prompt("Enter the number to find whether is divisible by 5 or not"));
+// if(gn%5==0){
+//     document.write("The given number is divisible by 5");
+// }
+// else{
+//     document.write("The given number is an not divisible by 5");
+
+// }
+
+// To find the number is a multiple of 7 
+
+// let gn=parseInt(prompt("Enter the number to find whether is multiple of 7 or not"));
+// if(gn%7==0){
+//     document.write("The given number is multiple of 7");
+// }
+// else{
+//     document.write("The given number is an not multiple of 7");
+
+// }
+
