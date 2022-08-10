62cf688a599dc1081dc7dbae08514a94c37a5f11
HarishHec7700 <harish.hariharan7@gmail.com>
2022-08-10 16:59:20 +0530

    Java homeworks

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-// // Area of circle
+// // 1 .Area of circle
 
 // let r = parseInt(prompt("Give radius"));
 // let a,bi;
@@ -8,7 +8,7 @@
 // alert(a);
 // document.write(a);
 
-// // Area of triangle
+// // 2. Area of triangle
 
 // let base,height,at;
 // base=parsefloat(prompt("Give the base value"));
@@ -18,7 +18,7 @@
 // alert(at);
 // document.write(at);
 
-// Simple Interest
+// 3. Simple Interest
 
 // let p,n,rate,si;
 // p=parseFloat(prompt("Give the Principal amount"));
@@ -28,7 +28,7 @@
 // console.log(si);
 // document.write(si);
 
-// Square , Cube
+// 4. Square , Cube
 
 // let sq,cu, ab=parseFloat(prompt("Give the number to find its square and cube "));
 // sq=ab**2;
@@ -38,7 +38,7 @@
 // console.log(cu);
 // document.write(cu);
 
-// Swap of two numbers 
+// 5. Swap of two numbers 
 
 // let x=parseFloat(prompt("Give the  first number")),y=parseFloat(prompt("Give the secound number")),z;
 // console.log(x);
@@ -51,7 +51,7 @@
 // console.log(y);
 // document.write("y=",y);
 
-// To calculate the total and average 
+// 6. To calculate the total and average 
 
 // let num=[],n,s=0,sum = 0,i,j;
 // n=parseInt(prompt("Enter the total terms"))
@@ -68,7 +68,7 @@
 // console.log(avg);
 // document.write("the average is ",avg);
 
-// Find the given number is even or odd
+// 7. Find the given number is even or odd
 
 // let gn=parseInt(prompt("Enter the Number to find its odd or even"));
 // if(gn%2==0){
@@ -79,7 +79,7 @@
 
 // }
 
-// To check whether the number is positive or negative
+// 8. To check whether the number is positive or negative
 
 // let gn=parseInt(prompt("Enter the Number to find its positive or negative"));
 // if(gn<0){
@@ -92,7 +92,7 @@
 //     document.write("The given number is negative number");
 // }
 
-// To find the number is divisible by 5 
+// 9. To find the number is divisible by 5 
 // let gn=parseInt(prompt("Enter the number to find whether is divisible by 5 or not"));
 // if(gn%5==0){
 //     document.write("The given number is divisible by 5");
@@ -102,7 +102,7 @@
 
 // }
 
-// To find the number is a multiple of 7 
+// 10. To find the number is a multiple of 7 
 
 // let gn2, gn1=parseInt(prompt("Enter the number to find whether is multiple of 7 or not"));
 // gn2=parseInt(prompt("Enter the number to find whether is multiple of 7 or not"));
@@ -131,7 +131,7 @@
 //     document.write("The biggest number is ",gn1);
 // }
 
-// To arrange in ascending and descending order for 3 numbers 
+// 11. To find the greatest of  3 numbers 
 
 // let a,b,c;
 // a=parseInt(prompt("Enter the first number"));
@@ -154,7 +154,7 @@
 //     document.write(" First number is greater");
 // }
 
-// Factorial of a number
+// 12. Factorial of a number
 
 // let fac=1,num,i;
 // num=parseInt(prompt("Enter the Number to find its Factorial"));
@@ -164,14 +164,102 @@
 // console.log(fac);
 // document.write(fac);
 
-// Fibnocci series for n terms 
+// 13. Fibnocci series for n terms 
 
-let i,n,f,fb;
-fb=f=0;
-n=parseInt(prompt("Enter the nth term u need to find fibanocci series"));
-for (let i = 1; i<n; i++) {
-    fb=i+(i+1);
-    // f=fb;
-    console.log(fb);
-    document.write(fb);
-}
+// let i,n,f,fb,c;
+// fb=1;f=0;
+// n=parseInt(prompt("Enter the nth term u need to find fibanocci series"));
+// console.log(f);
+// document.write(f);
+// console.log(fb);
+// document.write(fb);
+
+// for (let i = 1; i<n-1; i++) {
+//     c=f+fb;
+//     f=fb;
+//     fb=c;
+//     console.log(c);
+//     document.write(c);
+// }
+
+// 14 & 15. Print n even numbers an n odd numbers  
+
+// let n=parseInt(prompt("Enter the numbers under which u need to find the even numbers"));
+// for (let i = 0; i < n; i++) {
+//     if(i%2==0){
+//         console.log(i);
+//         document.write(i,"is an even number");
+//     }    
+// }
+
+// let m=parseInt(prompt("Enter the numbers under which u need to find the odd numbers"));
+// for (let i = 0; i < m; i++) {
+//     if(i%2==1){
+//         console.log(i);
+//         document.write(i,"is an odd number");
+//     }    
+// }
+
+// 16. To find wheather the number is prime or not 
+
+// let n=parseInt(prompt("Enter the number to find whether it is prime or not"));
+// for (let i = 2; i <=n ; i++) {
+//     if (n%i==0) {
+//         console.log(n ,"is not a prime number");
+//         document.write(n,"is not a prime number");           
+//         break;
+//     }
+// }
+//     console.log(n ,"is a prime number");
+//     document.write(n,"is a prime number");    
+    
+    
+
+// 17. To find the prime numbers in a range 
+
+
+// 18. Print given N numbers in sequence
+// let n = parseInt(prompt("Enter the number to print in sequence"));
+
+// for(let i=0;i<=n;i++){
+//     console.log(i);
+//     document.write(i, ",");
+// }
+
+// 19.Print given N numbers in  reverse sequence
+
+// let n = parseInt(prompt("Enter the number to print in reverse sequence"));
+
+// for(let i=n;i>=0;i--){
+//     console.log(i);
+//     document.write(i, ",");
+// }
+
+// Ascending and descending order of given three numbers
+
+// let a= parseFloat(prompt("Enter the first value")), b= parseFloat(prompt("Enter the first value")), c= parseFloat(prompt("Enter the first value"));
+// if(a>b){
+//     if(a>c){
+//         console.log(a);
+//     }
+//     else{
+//         console.log(c);
+//     }
+// }
+// if(b>c){
+//     if(b>a){
+//         console.log(b);
+//     }
+//     else{
+//         console.log(c);
+//     }
+// }                                                                xxxxxxx
+// if(c>a){
+//     if(c>b){
+//         console.log(c);
+//     }
+//     else{
+//         console.log(a);
+//     }
+// }
+
