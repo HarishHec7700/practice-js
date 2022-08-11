265aed62183fbb197de5087f5347193ae0e231d9
HarishHec7700 <harish.hariharan7@gmail.com>
2022-08-11 17:03:12 +0530

    Object

---
+++ Objects/Practises/obj-script.js
@@ -0,0 +1,156 @@
+let tws={
+    brand: "Boat",
+    model: 440,
+    color: "Bumblebee Yellow",
+    charging_time: 2,
+    playback_time:  3,
+} 
+console.log(tws);
+console.log(tws.brand);
+
+let notebook={
+    brand: "Diamond",
+    type: "double side ruled ",
+    total_pages: 150,
+}
+console.log(notebook);
+console.log(notebook["total_pages"]);
+
+let pen={
+    color: " blue and white",
+    ink_color:"Blue",
+    style: "with cap"
+}
+console.log(pen);
+console.log(pen["style"]);
+
+let switchboard={
+    material: "Plastic",
+    socket: 4,
+    switches: 2,
+}
+console.log(switchboard);
+console.log(switchboard["socket"]);
+
+let tubelight={
+    brand: "Orbit",
+    type: "LED",
+    color: "white",
+    power:"50 watts"
+}
+console.log(tubelight);
+console.log(tubelight["type"]);
+
+let plywood={
+    length:40,
+    thickness:5,
+    style: "with Mica",
+}
+console.log(plywood);
+console.log(plywood["style"]);
+
+let bag={
+    brand : "Roshan",
+    capacity:20,
+    style: "backbag", 
+}
+console.log(bag);
+console.log(bag["style"]);
+
+let tshirt={
+    color: "Blue",
+    collar_type:"Chinese",
+    material: "Demin"
+}
+console.log(tshirt);
+console.log(tshirt["material"]);
+
+let cake={
+    flavour: "Blackcurrent",
+    kg: 2,
+    shape: "round",
+}
+console.log(cake);
+console.log(cake["flavour"]);
+
+let bus={
+    company:"Volvo",
+    passenger_capacity:60,
+}
+console.log(bus);
+console.log(bus["company"]);
+
+let water_bottle={
+    material: "Steel",
+    capacity: "1 Litre",
+}
+console.log(water_bottle);
+console.log(water_bottle.capacity);
+
+let song={
+    genre:"rock",
+    lang:"English",
+}
+console.log(song);
+console.log(song.genre);
+
+let sketch={
+    color:12,
+    comp: "Strike line",
+    size: "small",
+}
+console.log(sketch);
+console.log(sketch.comp);
+
+let router={
+    comp:"T-Link",
+    serv_provider:"Railway Net",
+    users: 20, 
+}
+console.log(router);
+console.log(router.serv_provider);
+
+let charger={
+    comp: "Xiaomi",
+    Watts: 67,
+    type: "C",
+}
+console.log(charger);
+console.log(charger.comp);
+
+let keychain={
+    style:"Smiley",
+    color:"Yellow",
+}
+console.log(keychain);
+console.log(keychain.color);
+
+let mobile={
+    RAM: 4,
+    ROM: 128,
+    Comp: "Honor"
+}
+console.log(mobile);
+console.log(mobile.Comp);
+
+let lock={
+    type: "Lever",
+    Lever: 6,
+    material: "Steel",
+}
+console.log(lock);
+console.log(lock.material);
+
+let chart={
+    color:"yellow",
+    length: "30 inch",
+}
+console.log(chart);
+console.log(chart.length);
+
+let door={
+    ways: 2,
+    Wood: "Teak"
+}
+console.log(door);
+console.log(door.Wood);
