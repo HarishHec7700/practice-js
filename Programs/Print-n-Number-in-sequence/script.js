2f740fd4ae10d2ebb25b472c43a8fe5961f79644
HarishHec7700 <harish.hariharan7@gmail.com>
2022-08-30 14:52:46 +0530

    To print N numbers in sequence

---
+++ Programs/Print-n-Number-in-sequence/script.js
@@ -0,0 +1,7 @@
+//  Print given N numbers in sequence
+let n = parseInt(prompt("Enter the number to print in sequence"));
+
+for(let i=0;i<=n;i++){
+    console.log(i);
+    document.write(i, ",");
+}
