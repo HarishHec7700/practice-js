2673dbf91fb01577a880cb894511c8a3b0507bf6
HarishHec7700 <harish.hariharan7@gmail.com>
2022-09-13 23:21:11 +0530

    Divisible by 5

---
+++ Programs/Divisible of 5/script.js
@@ -0,0 +1,9 @@
+// 9. To find the number is divisible by 5 
+let gn=parseInt(prompt("Enter the number to find whether is divisible by 5 or not"));
+if(gn%5==0){
+    document.write("The given number is divisible by 5");
+}
+else{
+    document.write("The given number is an not divisible by 5");
+
+}
